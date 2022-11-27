b558f0e23b6b528a87bd5dc158c14421fee9e772
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2022-11-27 17:03:46 +0100

    Remove boilerplate and update packages

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,9 +15,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
     ];
 
     /**
@@ -27,7 +24,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
     }
 
     /**
