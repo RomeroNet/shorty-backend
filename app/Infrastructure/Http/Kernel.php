fa758bf3c85018aa50aa87a1aaf25c0670deccc9
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2022-11-30 03:36:02 +0100

    Create new folders and move files around

--- app/Infrastructure/Http/Kernel.php
+++ app/Infrastructure/Http/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http;
+namespace App\Infrastructure\Http;
 
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Auth\Middleware\Authorize;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Index;
+use App\Infrastructure\Http\Controllers\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
